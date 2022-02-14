e9bcfcd82780e25a45852177445e1cf6437127b6
ASUS <minhthoai190892@gmail.com>
2022-02-14 08:10:43 +0700

    intl

--- lib/pages/faker.dart
+++ lib/pages/faker.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_2_15/widgets/floating_action_button.dart';
 import 'package:faker/faker.dart';
-
+import 'package:intl/intl.dart';
+// ignore: must_be_immutable
+// ignore: must_be_immutable
 class FakerTest extends StatelessWidget {
   FakerTest({Key? key}) : super(key: key);
-  var faker = new Faker();
+  var faker = Faker();
   static const nameRoute = '/FakerTest';
   @override
   Widget build(BuildContext context) {
+    // ignore: avoid_print
     print(faker.person.name());
     return Scaffold(
       appBar: AppBar(
@@ -20,7 +23,7 @@ class FakerTest extends StatelessWidget {
             backgroundImage: NetworkImage("https://picsum.photos/id/${index+1}/200/300"),
           ),
           title: Text(faker.person.name()),
-          subtitle: Text(faker.lorem.sentence()),
+          subtitle: Text(DateFormat.yMMMd().format(DateTime.now())),
         ),
       ),
       floatingActionButton: const floatingActionButton(),
